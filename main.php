523ceed1aca1378971213c85149232385f2a2a7f
Mathias De Gucht <mathias.de.gucht@gmail.com>
2025-01-18 08:52:20 +0100

    updated require paths

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once './src/consts.php';
-require_once './src/funcs.php';
-require_once './src/flow.php';
-require_once './src/help.php';
+require_once __DIR__ . '/src/consts.php';
+require_once __DIR__ . '/src/flow.php';
+require_once __DIR__ . '/src/funcs.php';
+require_once __DIR__ . '/src/help.php';
 
 if ($argc > 1) {
     control_flow($argv);
